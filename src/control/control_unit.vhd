c11e47fa3ba7782408cfc288e98aa05e82d2fe66
luanzd <diegoluandenil@hotmail.com>
2026-04-23 16:06:44 -0300

    Added control_unit. OBS: Maybe u need reload u window to remove the 'No primary unit 'maindec' within library 'work''

--- src/control/control_unit.vhd
+++ src/control/control_unit.vhd
@@ -1,18 +1,56 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
 
 entity control_unit is
-  port(
-    instruction : in std_logic_vector(31 downto 0)
-      -- It's missing control signals!
-    );
+  port (
+    -- ENTRADAS (Fatias da instrução vindas do id_stage)
+    op         : in  std_logic_vector(6 downto 0); -- bits 6:0
+    funct3     : in  std_logic_vector(2 downto 0); -- bits 14:12
+    funct7b5   : in  std_logic;                    -- bit 30
+    
+    -- SAÍDAS (Sinais de Controlo para o Datapath)
+    ResultSrc  : out std_logic_vector(1 downto 0);
+    MemWrite   : out std_logic;
+    Branch     : out std_logic;
+    ALUSrc     : out std_logic;
+    RegWrite   : out std_logic;
+    Jump       : out std_logic;
+    ImmSrc     : out std_logic_vector(1 downto 0);
+    ALUControl : out std_logic_vector(2 downto 0)
+  );
 end entity control_unit;
 
-architecture rtl of control_unit is
+architecture struct of control_unit is
+
+  signal s_ALUOp : std_logic_vector(1 downto 0);
 
 begin
 
+  MD: entity work.maindec
+    port map (
+      op        => op,
+      
+      -- Sinais que vão direto para fora
+      ResultSrc => ResultSrc,
+      MemWrite  => MemWrite,
+      Branch    => Branch,
+      ALUSrc    => ALUSrc,
+      RegWrite  => RegWrite,
+      Jump      => Jump,
+      ImmSrc    => ImmSrc,
+      
+      -- Sinal interno que vai para o aludec
+      ALUOp     => s_ALUOp 
+    );
 
+  AD: entity work.aludec
+    port map (
+      opb5       => op(5),     -- Extrai o bit 5 do opcode para saber se é Tipo-R ou Tipo-I
+      funct3     => funct3,
+      funct7b5   => funct7b5,
+      ALUOp      => s_ALUOp,   -- Recebe o fio que veio do maindec
+      
+      ALUControl => ALUControl
+    );
 
-end architecture rtl;
+end architecture struct;
